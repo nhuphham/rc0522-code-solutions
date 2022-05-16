8aae72f09786389b64e738c6167f00b159cbdce6
“Nhu <nhu.phamx@yahoo.com>
2022-05-16 19:55:43 +0000

    javascript-operators-and-expressions

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,42 @@
+var width = 2;
+var height = 3;
+
+var area = width * height;
+console.log('value of area is', area);
+console.log('type of area is', typeof area);
+
+var bill = 30;
+var payment = 20;
+
+var change = bill - payment;
+console.log('value of change is', change);
+console.log('type of change is', typeof change);
+
+var quizzes = 50;
+var midterm = 30;
+var final = 40;
+
+var grade = (quizzes + midterm + final) / 3;
+console.log('value of grade', grade);
+console.log('type of grade', typeof grade);
+
+var firstName = 'nhu ';
+var lastName = 'pham';
+
+var fullName = firstName + lastName;
+console.log('value of fullName', fullName);
+console.log('type of fullName', typeof fullName);
+
+var pH = 3;
+var isAcidic = pH < 7;
+console.log('value of isAcidic', isAcidic);
+console.log('type of isAcidic', typeof isAcidic);
+
+var headCount = 5;
+var isSparta = headCount === 300;
+console.log('value of isSparta', isSparta);
+console.log('type of isSparta', typeof isSparta);
+
+var motto = fullName;
+console.log(motto, ' is the GOAT');
+console.log('type of motto', typeof motto);
