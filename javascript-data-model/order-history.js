c9e8a5a65fe25431540827a3029b96e8c713e472
“Nhu <nhu.phamx@yahoo.com>
2022-05-17 22:22:09 +0000

    javascript-data-model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,49 @@
+var orderHistory = [
+  {
+    name: 'Javascript for impatient programmers',
+    author: 'Rauschmayer, Dr. Axel',
+    orderNumber: '114-3941689-8772232',
+    orderPlaced: 'August 4, 2020',
+    orderDelivered: 'Aug 8, 2020',
+    recipient: 'JS masher',
+    price: 31.55
+  },
+  {
+    name: 'The Timeless way of building',
+    author: 'Alexander, Christopher',
+    orderNumber: '113-9984268-1280257',
+    orderPlaced: 'July 19, 2020',
+    orderDelivered: 'Jul 20, 2020',
+    recipient: 'JS masher',
+    price: 41.33
+  },
+  {
+    name: 'GameCube Controller Adapter. SSB switch GameCube Adapter for WII, etc. Support turbo and vibration features.',
+    author: null,
+    orderNumber: '114-2875557-9059409',
+    orderPlaced: 'July 4, 2020',
+    orderDelivered: 'Jul 7, 2020',
+    recipient: 'JS masher',
+    price: 15.98
+  },
+  {
+    orderNumber: '113-2883177-2648248',
+    orderPlaced: 'July 3, 2020',
+    orderDelivered: 'July 5, 2020',
+    recipient: 'JS masher',
+    total: 138.83,
+    items: [
+      {
+        name: 'GameCube controller- SSB. Edition',
+        author: null,
+        price: 94.95
+
+      },
+      {
+        name: 'The art of Sql',
+        author: 'Faroult, Stephane',
+        price: 33.99
+      }
+    ]
+  }
+];
