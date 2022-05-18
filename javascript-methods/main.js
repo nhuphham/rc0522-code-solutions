ceb3d8e1594a7b99f0847465a983b49814491a69
“Nhu <nhu.phamx@yahoo.com>
2022-05-18 22:40:24 +0000

    JS Methods in-progress

---
+++ javascript-methods/main.js
@@ -0,0 +1,13 @@
+var num = (1, 2, 3, 4, 5);
+var maxium = Math.max(num);
+console.log(maxium);
+
+var heros = ['batman', 'superman', 'ironman', 'spiderman'];
+var randomNumber = Math.random(heros);
+randomNumber = heros.length * randomNumber;
+var randomIndex = Math.floor(randomNumber);
+
+console.log(randomIndex);
+
+var randomHero = randomIndex.heros;
+console.log(randomHero);
