8cb3c93e93de8c3b52fcc7aba12e463fa3f8de0a
“Nhu <nhu.phamx@yahoo.com>
2022-05-19 07:22:44 +0000

    javascript-methods

--- javascript-methods/main.js
+++ javascript-methods/main.js
@@ -9,5 +9,49 @@ var randomIndex = Math.floor(randomNumber);
 
 console.log(randomIndex);
 
-var randomHero = randomIndex.heros;
+var randomHero = heros[randomIndex];
 console.log(randomHero);
+
+var library = [
+  {
+    title: 'The Power of Habit',
+    author: 'Charles Duhigg'
+  },
+  {
+    title: 'Pillow thoughts',
+    author: 'Pepper Nilla'
+  },
+  {
+    title: 'Milk and Honey',
+    author: 'Rupi Kour'
+  }
+];
+
+var js = {
+  title: 'JavaScript for Impatient Programmers',
+  author: 'Dr. Axel Rauschmayer'
+};
+var css = {
+  title: 'CSS Secrets',
+  author: 'Lea Verou'
+};
+
+var lastBook = library.pop();
+console.log(lastBook);
+var firstBook = library.shift();
+console.log(firstBook);
+
+library.push(js);
+library.unshift(css);
+library.splice(-1);
+console.log(library);
+
+var fullName = 'Nhu Pham';
+var split = fullName.split(' ');
+
+var firstAndLastName = split;
+console.log('firstAndLastName:', firstAndLastName);
+
+var firstName = firstAndLastName[0];
+var sayMyName = firstName.toUpperCase();
+console.log('sayMyName:', sayMyName);
