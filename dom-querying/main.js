162e453d8089c13dd24765629d9419c74c6e4a69
“Nhu <nhu.phamx@yahoo.com>
2022-05-23 22:47:29 +0000

    dom-querying

---
+++ dom-querying/main.js
@@ -0,0 +1,19 @@
+console.log('hello, world');
+
+const el = document.querySelector('h1');
+console.log(el);
+console.dir(el);
+
+const ex = document.querySelector('#explanation');
+console.log(ex);
+console.dir(ex);
+
+const hint = document.querySelector('.hint');
+console.log(hint);
+console.dir(hint);
+
+const example = document.querySelectorAll('p');
+console.log(example);
+
+const link = document.querySelectorAll('.example-link');
+console.log(link);
