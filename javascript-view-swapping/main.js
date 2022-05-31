6a50b5a57e019f7957d9e9718ddd1d3efdbb690f
“Nhu <nhu.phamx@yahoo.com>
2022-05-31 18:17:55 +0000

    completed

--- javascript-view-swapping/main.js
+++ javascript-view-swapping/main.js
@@ -1,28 +1,30 @@
-var tabContainer = document.querySelector('.tab-container');
-var views = document.querySelector('.view');
-var tabs = document.querySelector('.tab');
+const tabContainer = document.querySelector('.tab-container');
+const views = document.querySelectorAll('.view');
+const tabs = document.querySelectorAll('.tab');
 
-tabContainer.addEventListener('click', handleViewSwap());
+tabContainer.addEventListener('click', handleViewSwap);
 
 function handleViewSwap(event) {
   if (!event.target.matches('.tab')) {
     return;
   }
-  for (var tabIndex = 0; tabIndex < tabs.length; tabIndex++) {
-    if (tabs[tabIndex] === event.target) {
-      tabs[tabIndex].className = 'tab active';
+
+  for (let i = 0; i < tabs.length; i++) {
+    if (tabs[i] === event.target) {
+      tabs[i].className = 'tab active';
     } else {
-      tabs[tabIndex].className = 'tab';
+      tabs[i].className = 'tab';
     }
   }
 
   var viewName = event.target.getAttribute('data-view');
 
-  for (var viewIndex = 0; viewIndex < views.length; viewIndex++) {
-    if (views[viewIndex].getAttribute('data-view') === viewName) {
-      views[viewIndex].className = 'view hidden';
+  for (let j = 0; j < views.length; j++) {
+
+    if (views[j].getAttribute('data-view') !== viewName) {
+      views[j].className = 'view hidden';
     } else {
-      views[viewIndex].className = 'view';
+      views[j].className = 'view';
     }
   }
 
